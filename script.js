44a7c6738497c70eaa0b530f133c2374b15eec4d
Michał Barczyński <michal.barczynski97@gmail.com>
2023-02-13 22:05:09 +0100

    all tasks finished

--- script.js
+++ script.js
@@ -1,92 +1,83 @@
 // Swiper
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+  slidesPerView: 4,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 // Zip code validator
 var input = document.getElementById('zip-code'),
-    oldValue,
-    regex = new RegExp(/^\d{0,5}$/g),
-    mask = function(value) {
-      let output = [];
-        for(let i = 0; i < value.length; i++) {
-          if(i == 2) {
-            output.push("-");
-          }
-          output.push(value[i]);
-        }
-        return output.join("");
-    },
-    unmask = function(value) {/^\d{0,5}$/g
-      let output = value.replace(new RegExp(/[^\d]/, 'g'), '');
-      return output;
-    },
-    keydownHandler = function(e) {
-      oldValue = e.target.value;
-    },
-    inputHandler = function(e) {
-          let el = e.target,
-          newValue = el.value;
-          newValue = unmask(newValue);
-          
-          if(newValue.match(regex)) {
-            newValue = mask(newValue);
-            el.value = newValue;
-          } else {
-            el.value = oldValue;
-          }
-    };
-input.addEventListener('keydown', keydownHandler );
-input.addEventListener('input', inputHandler );
+  oldValue,
+  regex = new RegExp(/^\d{0,5}$/g),
+
+  mask = function (value) {
+    let output = [];
+    for (let i = 0; i < value.length; i++) {
+      if (i == 2) {
+        output.push("-");
+      }
+      output.push(value[i]);
+    }
+    return output.join("");
+  },
+  unmask = function (value) {
+    /^\d{0,5}$/g
+    let output = value.replace(new RegExp(/[^\d]/, 'g'), '');
+    return output;
+  },
+
+  keydownHandler = function (e) {
+    oldValue = e.target.value;
+  },
+
+  inputHandler = function (e) {
+    let el = e.target,
+      newValue = el.value;
+    newValue = unmask(newValue);
+
+    if (newValue.match(regex)) {
+      newValue = mask(newValue);
+      el.value = newValue;
+    } else {
+      el.value = oldValue;
+    }
+  },
+
+  isZipValid = function (e) {
+    let el = e.target.value;
+    if (el.length != 6) { alert('Kod pocztowy musi zawierać 5 cyfr!') }
+  }
+
+input.addEventListener('keydown', keydownHandler);
+input.addEventListener('input', inputHandler);
+input.addEventListener('change', isZipValid);
 
 // Fetching form inputs and sending to URL
+const form = document.querySelector('#contact-form');
+
 form.addEventListener("submit", e => {
   e.preventDefault();
 
-  let formErrors = false;
+  const prePayload = new FormData(form);
+  const payload = new URLSearchParams(prePayload);
+  console.log([...payload]);
 
-  //2 etap - sprawdzamy poszczególne pola gdy ktoś chce wysłać formularz
-  for (const el of inputs) {
-      markFieldAsError(el, false);
-      toggleErrorField(el, false);
+  const url = form.getAttribute("action");
+  const method = form.getAttribute("method");
 
-      if (!el.checkValidity()) {
-          markFieldAsError(el, true);
-          toggleErrorField(el, true);
-          formErrors = true;
-      }
-  }
-
-  if (!formErrors) {
-      const submit = form.querySelector("[type=submit]");
-      submit.disabled = true;
-      submit.classList.add("loading");
-
-      //generuję dane do wysyłki
-      const formData = new FormData(form);
-
-      const url = form.getAttribute("action"); //pobieramy adres wysyłki z action formularza
-      const method = form.getAttribute("method"); //tak samo metodę
-
-      fetch(url, {
-          method: method,
-          body: formData
-      })
-      .then(res => res.json())
-      .then(res => {
-          //tutaj odpowiedź
-      }).finally(() => { //gdy zakończy się połączenie chcemy włączyć przycisk submit
-          submit.disabled = false;
-          submit.classList.remove("loading");
-      });
-  }
+  fetch(url, {
+    method: method,
+    body: payload
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 });
+ 
